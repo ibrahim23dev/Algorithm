329e491266531ca244dd74228e5c693432dd825a
ibrahim23dev <ibrahim.soft.dev23@gmail.com>
2024-10-15 16:11:02 +0600

    JavaScript

---
+++ Script2.js
@@ -0,0 +1,36 @@
+// let number=(a,b)=>a+b;
+// console.log(number(20,20));
+
+// function helloThis(){
+//     'use strict';
+//     console.log(this)
+// }
+
+// helloThis();
+
+// function helloArrow = {
+//    name:"JavaScript",
+//    libaries: ["React","Vue","Angular"],
+//    PrintLibaries: function(){
+//     this.libaries.forEach((a)=>console.log(`${this.name} loves ${a}`))
+//    }
+// }
+// helloArrow.PrintLibaries();
+
+// const helloArrow = {
+//     name: "JavaScript",
+//     libaries: ["React", "Vue", "Angular"],
+//     PrintLibaries: function() {
+//       this.libaries.forEach((a) => console.log(`${this.name} loves ${a}`));
+//     }
+//   };
+  
+//   helloArrow.PrintLibaries();
+  
+
+//Ternary Operator
+var age=20
+
+var type = age >= 23 ? "adult" : age < 9 ? "Child" : "young";
+
+console.log(type);
